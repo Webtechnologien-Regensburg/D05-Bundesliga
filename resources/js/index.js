0a61f8baf9e680609203b1a5e22caf5560e9d15a
alexanderbazo <alexanderbazo@googlemail.com>
2022-06-07 13:49:25 +0200

    Prepare starter code

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,17 +1,11 @@
-/*
- * In dieser Datei wird der gesamte Update-Prozess implementiert, der beim Aufruf der HTML-Seite
- * dazu führt, dass die aktuelle Bundesligatabelle angezeigt wird. Dazu wird zuerst via SeasonRequest
- * die aktuelle Tabelle vom Server erfragt und in eine Array aus Team-Objekten umgewandelt. Mit diesen
- * Daten wird anschließend ein TableView erstellt bzw. befüllt, in dem die Bundesliga-Tabelle dargestellt wird. Dazu 
- * werden auf Basis der Team-Objekte einzelne Tabellen-Zeilen (td-Elemente) erstellt und als Kindelemente
- * des bereits existierenden table-Elements im DOM eingefügt. 
- * */
+/**
+ * Über diese URL können wir mit einer HTTP-Anfrage die Tabelle einer beliebigen Bundesligasaison
+ * abfragen. Die gewünschte Saison wird am Ende der URL angefügt. Als Antwort auf die Anfrage 
+ * erhalten wir die aktuelle Tabelle als strukturierten Datensatz. Das konkrete Format ist abhängig 
+ * von der Art der Anfrage. Im Browser werden uns die Informationen als XML-Datensatz angezeigt.
+ * Bei unserer programmatischen Anfrage im Quellcode werden wir dafür sorgen, dass die Daten im 
+ * (in JavaScript) besser verarbeitbaren JSON-Format übertragen werden.
+ */
+ const API_URL = "https://www.openligadb.de/api/getbltable/bl1/";
 
-const seasonRequest = new SeasonRequest("2021"); //  Erstellen des AJAX/HTTP-Requests-Wrappers
-
-// Ausführend des Requests durch den Aufruf der "run"-Methode
-seasonRequest.run((results) => { // Verarbeitung der Ergebnisse ("results") in der inline-definierten Callback-Methode
-    let tableElement = document.querySelector("table"), // Selektion des Tabellen-Elements aus dem DOM
-    tableView = new TableView(tableElement); // Erstellen des TableView-Objekts, das das Tabellen-Element verwaltet
-    tableView.show(results); // Anzeigen der vom Server erhaltenden und im SeasonRequest transformierten Ergebnisse in der Tabelle
-});
+ // TODO: Hier beginnt unsere Implementierung
